4d6c585650a21f8d7eff10672cda15e45eb3082f
bitzec <bitzec18@gmail.com>
2019-07-07 22:22:21 +0200

    up

--- config/daemon/locate-bitzec-conf.js
+++ config/daemon/locate-bitzec-conf.js
@@ -5,13 +5,13 @@ import os from 'os';
 
 import { app } from '../electron'; // eslint-disable-line
 
-export const locateCommerciumConf = () => {
+export const locateBitzecConf = () => {
   if (os.platform() === 'darwin') {
     return path.join(app.getPath('appData'), 'Bitzec', 'bitzec.conf');
   }
 
   if (os.platform() === 'linux') {
-    return path.join(app.getPath('home'), '.bitzec', 'commercium.conf');
+    return path.join(app.getPath('home'), '.bitzec', 'bitzec.conf');
   }
 
   return path.join(app.getPath('appData'), 'Bitzec', 'bitzec.conf');
