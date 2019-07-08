bb1e5db280b38886bafc51f94dde29fd021e8834
bitzec <bitzec18@gmail.com>
2019-07-08 18:47:54 +0200

    Update explorer.js

--- app/constants/explorer.js
+++ app/constants/explorer.js
@@ -4,4 +4,4 @@ import { isTestnet } from '../../config/is-testnet';
 
 export const BITZEC_EXPLORER_BASE_URL = isTestnet()
   ? 'https://chain.so/tx/BZCTEST/'
-  : 'http://http://35.242.189.203:3001/insight/tx/';
+  : 'http://35.242.189.203:3001/insight/tx/';
